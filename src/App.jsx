5bc37911cf31d46a8b3658cb0ffa16c50283beaa
JobGoog <roro425052@gmail.com>
2025-04-26 20:43:09 +0300

    test

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Stars from './Stars';
+
+export default function App() {
+  return (
+    <>
+      <h2>Пример рейтинга</h2>
+      <Stars count={3} />
+
+      {/* Дополнительные примеры для теста */}
+      <h2 style={{ marginTop: 32 }}>Другие варианты</h2>
+      <Stars count={5} />
+      <Stars count={1} />
+    </>
+  );
+}
